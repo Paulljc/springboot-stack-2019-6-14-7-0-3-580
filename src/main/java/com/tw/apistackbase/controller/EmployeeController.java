104908836511036c08c56a9ba589489281dbfbbb
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-16 15:50:49 +0800

    complete five require

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import com.tw.apistackbase.repository.EmpolyeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    EmpolyeeRepository repository = new EmpolyeeRepository();
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return repository.findAll();
+
+    }
+
+    @GetMapping("/employees/{empolyeeId}")
+    public Employee getEmployeesById(@PathVariable String empolyeeId){
+        return repository.findById(empolyeeId);
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return repository.addEmpolyee(employee);
+    }
+
+    @PostMapping("/employees{empolyeeId}")
+    public Employee deleteEmployeeById(@PathVariable String empolyeeId){
+        return repository.removeEmpolyee(empolyeeId);
+    }
+
+    @GetMapping(value = "/employees", params = "ageMini")
+    public List<Employee> findAgeAbove(@RequestParam int ageMini){
+        return repository.findAgeAbove(ageMini);
+    }
+}
